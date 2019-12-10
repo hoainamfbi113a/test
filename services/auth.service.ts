20dd22edf11bbed902e2ddbff1ccec930fb12c5d
TungMQ <tungmq@solazu.com>
2019-12-10 16:14:22 +0700

    format code

--- services/auth.service.ts
+++ services/auth.service.ts
@@ -1,33 +1,33 @@
 //#region Global Imports
-import {Context, ServiceSchema, Errors} from "moleculer";
+import { Context, ServiceSchema, Errors } from "moleculer";
 import { Action } from "moleculer-decorators";
 import { IAuth } from 'Interfaces';
-import {ModelOrganization, ModelUser} from "../src/models";
+import { ModelOrganization, ModelUser } from "../src/models";
 import jwtService from "BaseService/services/jwt.service";
 
-class AuthService implements ServiceSchema{
-	public name: string = 'auth';
+class AuthService implements ServiceSchema {
+    public name: string = 'auth';
 
     @Action({
-		body: {
-            email: { type: "string" },
+        params: {
+            email: { type: "email" },
             password: { type: "string", min: 1 }
-		},
-	})
-	public async login(ctx: Context<IAuth.Login>): Promise<any> {
-		const { email, password } = ctx.params;
-		const modelUser = new ModelUser(ctx);
+        },
+    })
+    public async login(ctx: Context<IAuth.Login>): Promise<any> {
+        const { email, password } = ctx.params;
+        const modelUser = new ModelUser(ctx);
         let user = await modelUser.getUserByEmail(email);
         if (!user) {
-            throw new Errors.MoleculerError('Email not register',402);
+            throw new Errors.MoleculerError('Email not register', 402);
         }
         if (!user.activated) {
-            throw new Errors.MoleculerError('user not active',402);
+            throw new Errors.MoleculerError('user not active', 402);
         }
 
         let check = await jwtService.compare(password, user.password);
         if (!check) {
-            throw new Errors.MoleculerError('Wrong password',402);
+            throw new Errors.MoleculerError('Wrong password', 402);
         }
 
         const orgModel = new ModelOrganization(ctx);
