573bc6be8a5686a7802cd881c74eb6e79d23da9d
TungMQ <tungmq@solazu.com>
2019-12-16 13:40:59 +0700

    check table existed before creating table

--- src/db/migrations/20191104003541_create_module_table.ts
+++ src/db/migrations/20191104003541_create_module_table.ts
@@ -1,15 +1,17 @@
 import * as Knex from "knex";
 
+const tableName = "module";
 export async function up(knex: Knex): Promise<any> {
-    const tableName = "module";
-    await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
-    await knex.schema.createTable(`${tableName}`, (table) => {
-        table.uuid("id").unique().notNullable().primary().defaultTo(knex.raw("uuid_generate_v4()"));
-        table.string("name");
-        table.boolean("is_deleted").notNullable().defaultTo(knex.raw("false"));
-        table.timestamp("created_at").notNullable().defaultTo(knex.fn.now());
-        table.timestamp("updated_at").notNullable().defaultTo(knex.fn.now());
-    });
+    if (!await knex.schema.hasTable(tableName)) {
+        await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+        await knex.schema.createTable(`${tableName}`, (table) => {
+            table.uuid("id").unique().notNullable().primary().defaultTo(knex.raw("uuid_generate_v4()"));
+            table.string("name");
+            table.boolean("is_deleted").notNullable().defaultTo(knex.raw("false"));
+            table.timestamp("created_at").notNullable().defaultTo(knex.fn.now());
+            table.timestamp("updated_at").notNullable().defaultTo(knex.fn.now());
+        });
+    }
 }
 
 // tslint:disable-next-line:no-empty
