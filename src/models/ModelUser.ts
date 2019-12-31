213bdd22d12feb1bc1c874e4872cedd3d5a71542
sonpt <sonpt@solazu.net>
2019-12-31 16:27:12 +0700

    MAPP-1286: active reset password

--- src/models/ModelUser.ts
+++ src/models/ModelUser.ts
@@ -1,22 +1,8 @@
-import { Model } from 'BaseService/db/Model';
-import { QueryCondition } from 'base-service/dist/interfaces/iModel';
+import { QueryCondition } from "base-service/dist/interfaces/iModel";
+import { Model } from "BaseService/db/Model";
+import uuid = require("uuid");
 
 export class User {
-    public initOrg($id: string, $org_id: string, $email: string, $first_name: string, $last_name: string, $password: string, $phone_number: string, $verify_code: string) {
-        this.init($id, $org_id, $email, $first_name, $last_name, $password, $phone_number, $verify_code);
-    }
-
-    public init($id: string, $org_id: string, $email: string, $first_name: string, $last_name: string, $password: string, $phone_number: string, $verify_code: string) {
-        this.id = $id;
-        this.org_id = $org_id;
-        this.email = $email;
-        this.first_name = $first_name;
-        this.last_name = $last_name;
-        this.password = $password;
-        this.phone_number = $phone_number;
-        this.verify_code = $verify_code;
-        this.activated = this.is_super = this.is_deleted = false;
-    }
 
     public id: string;
     public org_id: string;
@@ -33,34 +19,49 @@ export class User {
     public is_deleted: boolean;
     public created_at: Date;
     public updated_at: Date;
+    public initOrg($id: string, $org_id: string, $email: string, $first_name: string, $last_name: string, $password: string, $phone_number: string, $verify_code: string) {
+        this.init($id, $org_id, $email, $first_name, $last_name, $password, $phone_number, $verify_code);
+    }
+
+    public init($id: string, $org_id: string, $email: string, $first_name: string, $last_name: string, $password: string, $phone_number: string, $verify_code: string) {
+        this.id = $id;
+        this.org_id = $org_id;
+        this.email = $email;
+        this.first_name = $first_name;
+        this.last_name = $last_name;
+        this.password = $password;
+        this.phone_number = $phone_number;
+        this.verify_code = $verify_code;
+        this.activated = this.is_super = this.is_deleted = false;
+    }
 }
 
 export class ModelUser extends Model<User> {
     public tableName: string = "user";
 
     public async getUserByEmail(email: string): Promise<any> {
-        return await this.select('*').where({
-            email
+        return await this.select("*").where({
+            email,
         }).first();
     }
 
     public async isActiveVerifyCode(email: string, verifyCode: string): Promise<boolean> {
-        return await this.select('id').where({
-            email: email,
+        return await this.select("id").where({
+            email,
             activated: true,
             is_deleted: false,
-            verify_code: verifyCode
+            verify_code: verifyCode,
         }).first();
     }
 
     public async updateUserPassword(email: string, verifyCode: string, newPassword: string): Promise<boolean> {
-        let query: QueryCondition[] = new Array<QueryCondition>(
-            new QueryCondition('email', '=', email),
-            new QueryCondition('activated', '=', true),
-            new QueryCondition('is_deleted', '=', false),
-            new QueryCondition('verify_code', '=', verifyCode),
+        const query: QueryCondition[] = new Array<QueryCondition>(
+            new QueryCondition("email", "=", email),
+            new QueryCondition("activated", "=", true),
+            new QueryCondition("is_deleted", "=", false),
+            new QueryCondition("verify_code", "=", verifyCode),
         );
-        let result = await this.update({ password: newPassword, verify_code: null }, query, ['id']);
+        const result = await this.update({ password: newPassword, verify_code: uuid.v4() }, query, ["id"]);
         if (!result || (result && result.length == 0)) {
             return false;
         }
