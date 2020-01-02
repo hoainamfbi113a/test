a79fdfd31210fc208eb233ce8c4e8cb63ecbd3b4
TungMQ <tungmq@solazu.com>
2020-01-02 16:31:21 +0700

    MAPP-1190 add prefix route mobile

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,7 +1,9 @@
 import { apiRouter } from './api.router';
+import { mobileRouter } from './mobile.router';
 import { authRouter } from './auth.router';
 
 export const routers = [
-    {...apiRouter},
-    {...authRouter},
+    { ...apiRouter },
+    { ...authRouter },
+    { ...mobileRouter },
 ];
