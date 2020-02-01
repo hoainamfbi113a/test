dd46e4751a8cc7db04f694c471cd30bdeb95a70e
Pham Anh Tu <tupa@solazu.com>
2020-02-01 19:51:28 +0700

    Add slack log

--- moleculer.config.ts
+++ moleculer.config.ts
@@ -1,5 +1,7 @@
 "use strict";
 import brokerConfig from 'BaseService/config/moleculer.config'
+import SlackConfig from "src/utilities/slack";
+new SlackConfig().setup().on()
 const config = brokerConfig;
 config.nodeID = "service-core";
 module.exports = config;
