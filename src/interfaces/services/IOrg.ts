dedbf033f73b02aaaf2fd9d712178a0d240e1ddb
TungMQ <tungmq@solazu.com>
2020-02-05 11:56:31 +0700

    [MAPP-1066] apply tsconfig to fix import error

--- src/interfaces/services/IOrg.ts
+++ src/interfaces/services/IOrg.ts
@@ -1,19 +1,20 @@
-import { IBaseServiceQuery } from "base-service/dist/interfaces/common";
+import { IBaseServiceQuery } from "BaseService/interfaces/common";
 
+// tslint:disable-next-line: no-namespace
 export namespace IOrg {
-	export interface ICreateOrg extends IBaseServiceQuery {
-		first_name: string;
-		last_name: string;
-		phone_number: string;
-		company_name: string;
-		email: string;
-		password: string;
-		redirectUrl: string;
-	}
+  export interface ICreateOrg extends IBaseServiceQuery {
+    first_name: string;
+    last_name: string;
+    phone_number: string;
+    company_name: string;
+    email: string;
+    password: string;
+    redirectUrl: string;
+  }
 
-	export interface IActivateOrgInput extends IBaseServiceQuery {
-		verifyCode: string;
-		email: string;
-		orgId: string;
-	}
+  export interface IActivateOrgInput extends IBaseServiceQuery {
+    verifyCode: string;
+    email: string;
+    orgId: string;
+  }
 }
